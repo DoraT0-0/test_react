cddade9e66d08de22dea5c25fef73c0d2b2ed17d
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-09 00:03:05 +0300

    t_2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css'
 
 function App() {
-	return <div>
-		<p class = 'a'>1</p>
-        <p class = 'b'>2</p>
-        <p class = 'c'>3</p>
-	</div>;
+	return	<ul>
+		<li>1</li>
+		<li>2</li>
+		<li>3</li>
+		<li>4</li>
+		<li>5</li>
+		<li>6</li>
+		<li>7</li>
+		<li>8</li>
+		<li>9</li>
+		<li>10</li>
+		</ul>;
 }
 
 export default App;
