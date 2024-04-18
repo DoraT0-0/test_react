5c2b9be3b8e5fc7cedc1db76e74c9fca074d0dbb
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 13:27:37 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css'
+import React, { useState } from 'react';
+
 
 function App() {
-	return <input />;
-}
 
-export default App;
+	function num(num_){
+		let n = num_, b = 1
+		for(let i=0; i <= n; i++){
+			if(n % i == 0){
+				b *= i
+			}
+		}
+		return b
+	}
+
+	const [value1, setValue1] = useState(0);
+	const [result, setResult] = useState(0);
+	return <div>
+		<input value={value1} onChange={event => setValue1(event.target.value)} /> 
+		
+		<button onClick={() => setResult(num(value1))}>btn</button> 
+
+		<p>result: {result}</p>
+	</div>;
+}
+export default App 
