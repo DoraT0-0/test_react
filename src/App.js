43da1427600c9b8e8e523692efd0ffe88ad8cdde
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 11:55:55 +0300

    t_4

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css'
 
 function App() {
-	return(
-		<ul>
-			<li>text1</li>
-			<li>text2</li>
-			<li>text3</li>
-		</ul>
-	)
+	return <> 
+	<ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>
+	<ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>
+	</>
 }
 
 export default App;
