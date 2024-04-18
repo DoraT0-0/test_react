2db71e7f3d254cc74f946b27133934ac3d3e1725
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 18:29:13 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css'
+import React, { useState } from 'react';
+
 
 function App() {
-	return <input />;
+	const [value, setValue] = useState('');
+	
+	
+	return <div>
+		<select value={value} onChange={event => setValue(event.target.value)}>
+			<option>Москва</option>
+			<option>Екатеренбург</option>
+			<option>Омск</option>
+			<option>Воронеж</option>
+		</select>
+		<p>
+			ваш выбор: {value}
+		</p>
+	</div>;
 }
-
-export default App;
+export default App 
