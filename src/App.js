b63226089f020bb97bc67ed65b9d4be23f2cb35a
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 18:29:31 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css'
+import React, { useState } from 'react';
+
 
 function App() {
-	return <input />;
+	const texts = ['Москва', 'Екатеренбург', 'Омск', 'Воронеж'];
+	const [value, setValue] = useState('');
+	
+	const options = texts.map((text, index) => {
+		return <option key={index}>{text}</option>;
+	});
+	
+	return <div>
+		<select value={value} onChange={(event) => setValue(event.target.value)}>
+			{options}
+		</select>
+		<p>
+			ваш выбор: {value}
+		</p>
+	</div>;
 }
-
-export default App;
+export default App 
