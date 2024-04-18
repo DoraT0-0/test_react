89cf1ba3532b3a33f0b0e09610b00654a2d86026
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 12:05:29 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
 
 function App() {
-	return <input />;
+	const str1 = 'text1';
+	const str2 = 'text1';
+	
+	return <div>
+		<p>{str1}</p>
+		<p>{str2}</p>
+	</div>;
 }
-
 export default App;
