ea4a3f3c5affb886bc1bb8d58e1cb21e2c632450
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 13:00:33 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import './App.css'
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
 
 function App() {
-	return <input />;
-}
+	function id() {
+		const id = nanoid()
+		console.log(id);
+	}
 
+	const prods = [
+		{id: id(), name: 'product1', cost: 100},
+		{id: id(), name: 'product2', cost: 200},
+		{id: id(), name: 'product3', cost: 300},
+	];
+
+	const rows = prods.map(function(item) {
+		return <ul key={item.id}>
+			<li>{item.name}</li>
+			<li>{item.cost}</li>
+		</ul>;
+	});
+	
+	return <div>
+		{rows}
+	</div>;
+}
 export default App;
