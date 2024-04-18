7e73681a8e6dc94672a31efc223a6ee2795d6c9b
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 18:31:07 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-	return <input />;
+function getSum(arr) {
+	let sum = 0, len = 0;
+	
+	for (const elem of arr) {
+		sum += +elem;
+		len++
+	}
+	
+	return sum/len;
 }
 
-export default App;
+function App() {
+	const [notes, setNotes] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+	
+	function changeHandler(index, event) {
+		setNotes([...notes.slice(0, index), 
+			event.target.value, ...notes.slice(index + 1)]); 
+	}
+	
+	const result = notes.map((note, index) => {
+		return <input
+			key={index}
+			value={note}
+			onChange={event => changeHandler(index, event)}
+		/>;
+	});
+	
+	return <div>
+		{result}
+		<br/>
+		{getSum(notes)}
+	</div>;
+}
+export default App 
