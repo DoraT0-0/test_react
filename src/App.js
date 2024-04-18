18a842fbe56d7e9983dd7c17dc12da3d81e67b9c
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 13:03:42 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-	return <input />;
+function Celsia(num) {
+	return 5/9*(num - 32);
 }
 
-export default App;
+function App() {
+	const [F, setF] = useState(0);
+	
+	function handleChange(event) {
+		setF(event.target.value);
+	}
+	
+	return <div>
+		<input value={F} onChange={handleChange} />
+		<p>{Celsia(F)}</p>
+	</div>;
+}
+export default App
