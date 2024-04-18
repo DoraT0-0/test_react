76dfc156736c0b6391c7084851a25ce8a555af8a
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 12:23:42 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css'
 
 function App() {
-	return <input />;
-}
+	const li1 = <li>text1</li>;
+	const li2 = <li>text2</li>;
+	const li3 = <li>text3</li>;
 
+	return <ul>
+		{li1}
+		{li2}
+		{li3}
+	</ul>
+}
 export default App;
