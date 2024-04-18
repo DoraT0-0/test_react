6aba36f957279e63cf8c0840b44c12406fcb0a93
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 13:01:25 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css'
+import React, { useState } from 'react';
 
 function App() {
-	return <input />;
-}
-
+	const [name, setName] = useState('Name');
+	const [sur, setSur] = useState('Sur');
+	
+	return <div>
+		<span>{name}</span>
+		<br/>
+		<span>{sur}</span>
+		<br/>
+		<button onClick={() => {setName('name'); setSur('sur')}}>btn</button>
+	</div>;
+};
 export default App;
