83af597a33ae7ecd0b6b231a980132a23031a606
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 12:53:58 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css'
 
 function App() {
-	return <input />;
-}
+	function showMess(text) {
+		alert(text);
+	}
 
+	return <div>
+		<button onClick={showMess(1)}>act1</button>
+		<button onClick={showMess(2)}>act2</button>
+		<button onClick={showMess(3)}>act3</button>
+	</div>;
+}
 export default App;
