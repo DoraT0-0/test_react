74afcd99dfafad9c3d69b346c2016248670ffb7b
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 12:59:37 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css'
+import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
 
 function App() {
-	return <input />;
+	function func() {
+		const id = nanoid()
+		console.log(id);
+	}
+	
+	return <div>
+		<button onClick={func}>act</button> 
+	</div>;
 }
-
 export default App;
