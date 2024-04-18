de89ed6d11c6ee3148018125a429f79166e7c230
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 12:06:11 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css'
 
 function App() {
-	return <input />;
+	const arr = [1, 2, 3, 4, 5];
+	return(
+		<ul>
+			<li>{arr[0]}</li>
+			<li>{arr[1]}</li>
+			<li>{arr[2]}</li>
+			<li>{arr[3]}</li>
+			<li>{arr[4]}</li>
+		</ul>
+	)
 }
-
 export default App;
