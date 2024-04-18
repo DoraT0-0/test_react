95aec35d9b0be435b9d3f8cb5f84152dbf3315b4
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 13:02:55 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import React, { useState } from 'react';
 
 function App() {
-	return <input />;
+	const [value, setValue] = useState('');
+	
+	return <div>
+		<input value={value} onChange={event => setValue(event.target.value)} /> 
+		<p>len_text: {value.length}</p>
+	</div>;
 }
-
-export default App;
+export default App
