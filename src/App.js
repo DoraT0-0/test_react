0cd9747d3222ba3e9a19673c6e721a412475833c
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 12:48:00 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css'
 
 function App() {
-	const isAdmin = true;
-	if(isAdmin){
-		return <div>
-			<p></p>
-			<p></p>
-		</div>
-	}
+	const age = 19;
+
+	return <div>
+		{age>18 ? <p>true</p> : <p>false</p>}
+	</div>;
 	
 }
 export default App;
