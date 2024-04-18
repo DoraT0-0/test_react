c22985cd6c26a6836b1377e4eef6543cdb576f18
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 18:27:46 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css'
+import React, { useState } from 'react';
+
 
 function App() {
-	return <input />;
+	const [checked, setChecked] = useState(true);
+	
+	let message;
+	if (checked) {
+		message = 
+		<div>
+			<h2>Ура, вам уже есть 18</h2>
+			<p>
+				здесь расположен контент только для взрослых
+			</p>
+		</div>;
+	} else {
+		message = 
+		<div>
+	</div>;
+	}
+	
+	return <div>
+		<input type="checkbox" checked={checked} 
+			onChange={() => setChecked(!checked)} /> 
+		<div>{message}</div>
+	</div>;
 }
-
-export default App;
+export default App 
