a1d468d5e71da3f1374ac4582860b7055f9d9c00
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 18:55:59 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import React, { useState } from 'react';
 
 function App() {
 	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
-	
+	const [value, setValue] = useState('')
+
 	const result = notes.map((note, index) => {
 		return <li key={index}>{note}</li>;
 	});
 	
-	function changeHandler(){
-		let copy = [...notes, notes[notes.length - 1]+1];
+	function changeHandler(value){
+		let copy = [...notes, value];
 		setNotes(copy);
 	}
-
 	return <div>
 		<ul>
-			<button onClick={() => changeHandler(0)}>+</button>
+			<input value={value} onChange={event => setValue(event.target.value)}/>
+			<button onClick={() => changeHandler(value)}>+</button>
 			{result}
 		</ul>
 	</div>;
