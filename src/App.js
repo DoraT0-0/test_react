2a6b43f575e54d7a4bfb83352f21d317fb3df9c9
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 18:30:15 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import './App.css'
+import React, { useState } from 'react';
+
 
 function App() {
-	return <input />;
+	const [value, setValue] = useState(1);
+	
+	function changeHandler(event) {
+		setValue(event.target.value);
+	}
+	
+	return <div>
+		<input
+			type="radio"
+			name="radio"
+			value="1"
+			checked={value === '1' ? true : false}
+			onChange={changeHandler}
+		/>
+		<input
+			type="radio"
+			name="radio"
+			value="2"
+			checked={value === '2' ? true : false}
+			onChange={changeHandler}
+		/>
+		<input
+			type="radio"
+			name="radio"
+			value="3"
+			checked={value === '3' ? true : false}
+			onChange={changeHandler}
+		/>
+		<p>{value}</p>
+	</div>
 }
-
-export default App;
+export default App 
