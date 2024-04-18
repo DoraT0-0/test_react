52f64db955c185cb9b6504e79f82159c868df655
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 13:04:29 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css'
+import React, { useState } from 'react';
+
 
 function App() {
-	return <input />;
-}
+	const [value1, setValue1] = useState(0);
+	const [value2, setValue2] = useState(0);
+	const [result, setResult] = useState(0);
+	
+	return <div>
+		<input value={value1} onChange={event => setValue1(event.target.value)} /> 
+		<input value={value2} onChange={event => setValue2(event.target.value)} /> 
+		
+		<button onClick={() => setResult(Number(value1) + Number(value2))}>btn+</button> 
+		<button onClick={() => setResult(Number(value1) * Number(value2))}>btn*</button> 
 
-export default App;
+		<p>result: {result}</p>
+	</div>;
+}
+export default App
