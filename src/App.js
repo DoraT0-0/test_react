0999205c19efeae88a750c4f8fa3915137fd8312
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 12:53:32 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import './App.css'
 
 function App() {
-	return <input />;
+	function show1() {
+		alert(1);
+	}
+	
+	function show2() {
+		alert(2);
+	}
+	
+	return <div>
+		<button onClick={show1}>act1</button>
+		<button onClick={show2}>act2</button>
+	</div>;
 }
-
 export default App;
