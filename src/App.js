47fcf1fa01d6d7a5515dd8458ae653f967850aaa
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 18:30:46 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import React, { useState } from 'react';
+
 
 function App() {
-	return <input />;
+	const [value, setValue] = useState('text');
+	
+	return <div>
+		<input defaultValue={value} />
+	</div>;
 }
-
-export default App;
+export default App 
