aa24ea28d6c9b1340542f3c1234a4e6a56e4a621
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 13:03:20 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css'
+import React, { useState } from 'react';
 
-function App() {
-	return <input />;
+function age_(num) {
+	return 2024-num;
 }
 
-export default App;
+function App() {
+	const [age, setAge] = useState(0);
+	
+	function handleChange(event) {
+		setAge(event.target.value);
+	}
+	
+	return <div>
+		<input value={age} onChange={handleChange} />
+		<p>{age_(age)}</p>
+	</div>;
+}
+export default App
