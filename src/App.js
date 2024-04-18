cc7ca0bf77b8b4f7bfca56a58fcdb9a155fd3737
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 18:30:28 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import './App.css'
+import React, { useState } from 'react';
+
 
 function App() {
-	return <input />;
+	const [value, setValue] = useState(1);
+	
+	function changeHandler(event) {
+		setValue(event.target.value);
+	}
+	
+	return <div>
+		<span>Python</span>
+		<input
+			type="radio"
+			name="radio"
+			value="Python"
+			checked={value === 'Python' ? true : false}
+			onChange={changeHandler}
+		/>
+		<br/>
+		<span>JavaScript</span>
+		<input
+			type="radio"
+			name="radio"
+			value="JavaScript"
+			checked={value === 'JavaScript' ? true : false}
+			onChange={changeHandler}
+		/>
+		<br/>
+		<span>C++</span>
+		<input
+			type="radio"
+			name="radio"
+			value="C++"
+			checked={value === 'C++' ? true : false}
+			onChange={changeHandler}
+		/>
+		<p>{value === 'JavaScript' ? value + ' Молодец!' :value}</p>
+	</div>
 }
-
-export default App;
+export default App 
