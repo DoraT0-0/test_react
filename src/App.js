75083ea65b66eb99b5908bcfce8f6c7fea8b8afe
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 12:57:10 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
 
 function App() {
-	return <input />;
+	function func(arg1, arg2, event) {
+		console.log(arg1, arg2, event);
+	}
+	
+	return <div>
+		<button onClick={event => func('eee', event,'eee2')}>act</button> 
+	</div>;
 }
-
 export default App;
