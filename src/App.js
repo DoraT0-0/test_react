3dab7e22a7763e4381a7ce5fde97620dc145ef71
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 19:44:08 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -4,19 +4,22 @@ import React, { useState } from 'react';
 function App() {
 	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
 	const [value, setValue] = useState('')
-	function doSmth(index) {
-		setValue(notes[index])
+	const [i, seti] = useState(0)
+	function doSmth(value) {
+		let copy = [...notes]
+		copy[i] = value
+		setNotes(copy)
 	}
 
 	const result = notes.map((note, index) => {
-		return <li key={index} onClick = {() => doSmth(index)}>
+		return <li key={index} onClick={() => {seti(index)}}>
 			{note}
 		</li>;
 	});
 	
 	return <div>
 		<ul>
-			<input value={value} onChange={event => setValue(event.target.value)}/>
+			<input value={value} onBlur={() => doSmth(value)} onChange={event => setValue(event.target.value)}/>
 			{result}
 		</ul>
 	</div>;
