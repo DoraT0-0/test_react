a1e13cd9a6623c1db4ea598ebde5cbce05db0945
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 14:21:09 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css'
+import React, { useState } from 'react';
+
 
 function App() {
-	return <input />;
-}
+	const [value, setValue] = useState('');
+	
+	function sum(val){
+		let n = val, sum = 0
+		while(n !== 0){
+			sum += n % 10
+			n = Math.floor(n/10)
+		}
+		return sum
+	}
 
-export default App;
+	return <div>
+		<textarea value={value} onChange={event => setValue(event.target.value)} /> 
+		<p>{sum(value)}</p>
+	</div>;
+}
+export default App 
