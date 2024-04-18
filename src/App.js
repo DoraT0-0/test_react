16c9e63056dac5e918ec65b534512d3e1b46c9a7
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 12:58:51 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css'
 
 function App() {
-	return <input />;
+	const users = [
+		{name: 'user1', surn: 'surn1', age: 30},
+		{name: 'user2', surn: 'surn2', age: 31},
+		{name: 'user3', surn: 'surn3', age: 32},
+	];
+
+	const res = users.map(function(item) {
+		return <li key={item.id}>
+			<span>{item.name}</span>
+			<span>{item.surn}</span>
+			<span>{item.age}</span>
+		</li>;
+	});
+
+	return <ul>
+		{res}
+	</ul>;
 }
 
 export default App;
