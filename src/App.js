a76b0bb1bf08630e0f9f025e6669153d01afa481
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-04-18 18:35:57 +0300

    ---

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
 import './App.css'
 import React, { useState } from 'react';
 
-function getSum(arr) {
-	let sum = 0, len = 0;
-	
-	for (const elem of arr) {
-		sum += +elem;
-		len++
-	}
-	
-	return sum/len;
+const initDate = {
+	year:  2025,
+	month: 12,
+	day:   31,
 }
 
 function App() {
-	const [notes, setNotes] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-	
-	function changeHandler(index, event) {
-		setNotes([...notes.slice(0, index), 
-			event.target.value, ...notes.slice(index + 1)]); 
+	function getDayOfWeek(date) {
+		const dayOfWeek = new Date(date).getDay();
+		return isNaN(dayOfWeek) ? null :
+		  ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
 	}
-	
-	const result = notes.map((note, index) => {
-		return <input
-			key={index}
-			value={note}
-			onChange={event => changeHandler(index, event)}
-		/>;
-	});
-	
+	const [obj, setObj] = useState(initDate);
+	let data_ = obj.year+'-'+obj.month+'-'+obj.day
+	function handleChange(prop, event) {
+		setObj({...obj, ...{[prop]: event.target.value}});
+	}
 	return <div>
-		{result}
-		<br/>
-		{getSum(notes)}
+		<input value={obj.day} onChange={event => handleChange('day', event)} /> 
+		<input value={obj.month} onChange={event => handleChange('month', event)} /> 
+		<input value={obj.year} onChange={event => handleChange('year', event)} /> 
+		
+		<br />
+		<p>{getDayOfWeek(data_)}</p>
 	</div>;
 }
 export default App 
