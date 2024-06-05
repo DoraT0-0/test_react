fb7bc116a264968f8ec137f63beee6bfcf4389c7
DoraT0-0 <116023170+DoraT00@users.noreply.github.com>
2024-06-05 12:26:10 +0300

    t_59_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import './App.css'
 import React, { useState } from 'react';
 
+const initNotes = [
+	{
+		id: 'GYi9G_uC4gBF1e2SixDvu',
+		prop1: 'value11',
+		prop2: 'value12',
+		prop3: 'value13',
+	},
+	{
+		id: 'IWSpfBPSV3SXgRF87uO74',
+		prop1: 'value21',
+		prop2: 'value22',
+		prop3: 'value23',
+	},
+	{
+		id: 'JAmjRlfQT8rLTm5tG2m1L',
+		prop1: 'value31',
+		prop2: 'value32',
+		prop3: 'value33',
+	},
+];
+
 function App() {
-	const [notes, setNotes] = useState([1, 2, 3, 4, 5]);
+	const [notes, setNotes] = useState(initNotes);
+	
 	function doSmth() {
-		let copy = [...notes]
-		copy.reverse()
-		setNotes(copy)
+		let copy = [...notes, {
+			id: 'JAmjRlfQT8rLTm5tG2m12',
+			prop1: 'value31',
+			prop2: 'value32',
+			prop3: 'value33',}];
+		setNotes(copy);
 	}
 
-	const result = notes.map((note, index) => {
-		return <li key={index}>
-			{note}
+	const result = notes.map(note => {
+		return <li key={note.id}>
+			<span>{note.prop1}</span>
+			<span>{note.prop2}</span>
+			<span>{note.prop3}</span>
 		</li>;
 	});
 	
 	return <div>
 		<ul>
-			<button onClick={() => doSmth()}>reverse</button>
 			{result}
 		</ul>
+		<button onClick={doSmth}>+</button>
 	</div>;
 }
+
 export default App 
